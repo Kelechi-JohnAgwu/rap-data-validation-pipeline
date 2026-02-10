cfe6b9bfd21d0f859bc36ec3bab216c393259882
Kelechi John Agwu <kelechiagwu16@gmail.com>
2026-02-10 23:24:56 +0000

    Update helpers_files.R

--- R/helpers_files.R
+++ R/helpers_files.R
@@ -1,1 +1,14 @@
+# File helpers -------------------------------------------------------------
+
+source("R/00_setup.R")
+
+load_raw_files <- function(path = here("data", "raw")) {
+
+  files <- list.files(
+    path,
+    full.names = TRUE
+  )
+
+  return(files)
+}
 

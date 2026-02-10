271b1727f4e166081255d81ce3d27654828e3fed
Kelechi John Agwu <kelechiagwu16@gmail.com>
2026-02-10 23:20:37 +0000

    Update 01_load_raw_data.R

--- R/01_load_raw_data.R
+++ R/01_load_raw_data.R
@@ -2,10 +2,16 @@
 
 source("R/00_setup.R")
 
-# Example: list files in data/raw
-raw_files <- list.files(
-  here("data", "raw"),
-  full.names = TRUE
-)
+load_raw_files <- function(path = here("data", "raw")) {
 
+  files <- list.files(
+    path,
+    full.names = TRUE
+  )
+
+  return(files)
+}
+
+# Run once interactively
+raw_files <- load_raw_files()
 raw_files

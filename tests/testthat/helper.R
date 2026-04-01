b26366ff5f8c8d319715a52382447b88a02dec70
Kelechi-JohnAgwu <kelechiagwu16@gmail.com>
2026-04-01 22:20:55 +0100

    test: source validate functions via getwd() in helper.R

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,3 +1,8 @@
 library(dplyr)
 library(tibble)
 library(purrr)
+
+
+# Load validate functions
+source(file.path(getwd(), "R/validate_duplicates.R"))
+source(file.path(getwd(), "R/validate_missing.R"))

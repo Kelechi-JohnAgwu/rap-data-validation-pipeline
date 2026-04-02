2a73e05b0ff316c18330737aedd28dc804f9f404
Kelechi-JohnAgwu <kelechiagwu16@gmail.com>
2026-04-02 18:05:35 +0100

    test: use rprojroot to resolve project root in helper.R

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -4,5 +4,7 @@ library(purrr)
 
 
 # Load validate functions
-source(file.path(getwd(), "R/validate_duplicates.R"))
-source(file.path(getwd(), "R/validate_missing.R")) 
+root <- rprojroot::find_root(rprojroot::has_file("DESCRIPTION"))
+source(file.path(root, "R/validate_duplicates.R"))
+source(file.path(root, "R/validate_missing.R"))  
+
